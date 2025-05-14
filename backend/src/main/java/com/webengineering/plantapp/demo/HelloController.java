6a505ff29fb7e70004f5b6421dcea8d1381300e4
steju4 <julian.stengele@gmail.com>
2025-05-14 12:56:08 +0200

    Initialize Spring Boot application with basic structure and configurations

---
+++ backend/src/main/java/com/webengineering/plantapp/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.webengineering.plantapp.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
---
+++ backend/src/test/java/com/webengineering/plantapp/demo/PlantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webengineering.plantapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
