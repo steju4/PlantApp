7d2ad9d4b7ab4edbe6d1cf2eb6460b956d3b0c4f
steju4 <julian.stengele@gmail.com>
2025-05-19 10:24:45 +0200

    Aktualisierung der CORS-Konfiguration zur Verwendung von allowedOriginPatterns anstelle von allowedOrigins

--- backend/src/main/java/com/webengineering/plantapp/backend/config/CorsConfig.java
+++ backend/src/main/java/com/webengineering/plantapp/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8100")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
